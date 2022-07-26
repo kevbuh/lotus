274a01254f32228f3181408bbaa8ea7403e45b54
Kevin Buhler <kevinbuhlerr@gmail.com>
2022-07-25 20:26:06 -0600

    init hero page

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lotus: "#e6007a",
+        petal: "#50e3c2 ",
+      },
+    },
+    fontFamily: {
+      sans: ["-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto"],
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [],
+  },
+  extend: {
+    animation: {
+      text: "text 5s ease infinite",
+    },
+    keyframes: {
+      text: {
+        "0%, 100%": {
+          "background-size": "200% 200%",
+          "background-position": "left center",
+        },
+        "50%": {
+          "background-size": "200% 200%",
+          "background-position": "right center",
+        },
+      },
+    },
+  },
+};
